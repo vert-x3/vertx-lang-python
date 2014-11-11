df60d022e21d09d4272d68540a0b6f54a0ab57be
Jordan Halterman <jordan.halterman@gmail.com>
2014-11-10 19:32:13 -0800

    Add initial Python code generation template.

---
+++ src/main/java/io/vertx/lang/python/PythonVerticleFactory.java
@@ -0,0 +1,123 @@
+package io.vertx.lang.python;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Verticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxException;
+import io.vertx.core.spi.VerticleFactory;
+
+import java.lang.ProcessBuilder.Redirect;
+
+import py4j.GatewayServer;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class PythonVerticleFactory implements VerticleFactory {
+
+  private Vertx vertx;
+  private GatewayServer gateway;
+  private int port = 25333;
+  private int client;
+
+  @Override
+  public void init(Vertx vertx) {
+    this.vertx = vertx;
+  }
+
+  public Vertx getVertx() {
+    return vertx;
+  }
+
+  @Override
+  public String prefix() {
+    return "python";
+  }
+
+  @Override
+  public Verticle createVerticle(String verticleName, ClassLoader classLoader) throws Exception {
+    return new PythonVerticle(verticleName);
+  }
+
+  public class PythonVerticle extends AbstractVerticle {
+
+    private final String verticleName;
+
+    private PythonVerticle(String verticleName) {
+      this.verticleName = verticleName;
+    }
+
+    private Process process;
+    private boolean asyncStart;
+    private Future<Void> startFuture;
+    private Future<Void> stopFuture;
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+      init();
+      ProcessBuilder pb = new ProcessBuilder("python", verticleName, String.valueOf(port), String.valueOf(client));
+      pb.redirectOutput(Redirect.INHERIT);
+      pb.redirectError(Redirect.INHERIT);
+      process = pb.start();
+      if (asyncStart) {
+        this.startFuture = startFuture;
+      } else {
+        startFuture.complete();
+      }
+    }
+
+    @Override
+    public void stop(Future<Void> stopFuture) throws Exception {
+      if (process != null) {
+        process.destroy();
+      }
+      stopFuture.complete();
+    }
+
+    public void started(boolean started) {
+      if (startFuture != null) {
+        if (started) {
+          startFuture.complete();
+        }
+      } else if (!started) {
+        asyncStart = true;
+      }
+    }
+
+    public void stopped(boolean stopped) {
+      if (stopFuture != null) {
+        if (stopped) {
+          stopFuture.complete();
+        }
+      }
+    }
+  }
+
+  @Override
+  public void close() {
+    if (gateway != null) {
+      gateway.shutdown();
+    }
+  }
+
+  private synchronized void init() {
+    if (gateway == null) {
+      boolean connected = false;
+      while (!connected) {
+        try {
+          gateway = new GatewayServer(this, port);
+          client = gateway.getCallbackClient().getPort();
+          gateway.start();
+          connected = true;
+        } catch (Exception e) {
+          port++;
+        }
+        if (port > 25340) {
+          throw new VertxException("Failed to bind to port");
+        }
+      }
+    }
+  }
+
+}
---
+++ src/main/resources/vertx_python/util.py
@@ -0,0 +1,68 @@
+import json
+from py4j.java_gateway import is_instance_of
+
+java_gateway = None
+jvm = None
+jvertx = None
+
+def vertx_init():
+    """Initializes the Vert.x connection."""
+    global java_gateway
+    global jvm
+    global jvertx
+
+    if not hasattr(__builtins__, 'jvm'):
+        import sys
+        try:
+            port = sys.argv[1]
+        except IndexError:
+            raise RuntimeError("Failed to connect to Vert.x")
+        try:
+            from py4j.java_gateway import JavaGateway, GatewayClient
+            java_gateway = JavaGateway(GatewayClient(port=int(port)))
+            jvm = java_gateway.jvm
+            jvertx = java_gateway.entry_point.getVertx()
+        except ImportError:
+            raise RuntimeError("Failed to connect to Vert.x. Py4J must be on your PYTHONPATH")
+
+def json_to_python(obj):
+    """Converts a Java JSON object to Python dict or list"""
+    return json.loads(obj.encode()) if obj is not None else None
+
+def list_obj_to_python(obj, type):
+    """Converts a Java iterable of objects to a Python list"""
+    if obj is None:
+        return None
+    result = []
+    iterator = obj.iterator()
+    while iterator.hasNext():
+        val = iterator.next()
+        result.append(type(json.loads(val.encode())) if val is not None else None)
+    return result
+
+def list_to_json(obj):
+    """Converts a Python list to Java JsonArray"""
+    return jvm.io.vertx.core.json.JsonArray(json.dumps(obj)) if obj is not None else None
+
+def dict_to_json(obj):
+    """Converts a Python dictionary to Java JsonObject"""
+    return jvm.io.vertx.core.json.JsonObject(json.dumps(obj)) if obj is not None else None
+
+def python_to_java(obj):
+    """Converts an arbitrary Python object to Java"""
+    if obj is None:
+        return None
+    if isinstance(obj, dict):
+        return dict_to_json(obj)
+    elif isinstance(obj, list):
+        return list_to_json(obj)
+    return obj
+
+def java_to_python(obj):
+    """Converts an arbitrary Java object to Python"""
+    if obj is None:
+        return None
+    elif is_instance_of(obj, jvm.io.vertx.core.json.JsonObject) or is_instance_of(obj, jvm.io.vertx.core.json.JsonArray):
+        return json_to_python(obj)
+    else:
+        return obj
